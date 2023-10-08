f3e77d6223e5e0e2b1c73b7fa750699b32ba6f9a
Rafique Ahmed <142241546+rafiquemangi@users.noreply.github.com>
2023-10-08 10:49:04 -0700

    Add files via upload

---
+++ word_c.ts
@@ -0,0 +1,16 @@
+import inquirer from 'inquirer'
+
+const counter=(paragrah:string) => paragrah.replace(/\s/g,"").length
+
+async function startWordCount(counter:(text:string)=>number) {
+    do{
+        let res = await inquirer.prompt({
+            type:"input",
+            message:"write paragraph here...",
+            name:"paragraph",
+        })
+        console.log(counter(res.paragraph))
+    }while(true)
+
+}
+startWordCount(counter)
